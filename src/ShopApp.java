cdb807a417917dabe2c415c4eb46af7bc7ea83bc
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2021-10-12 20:21:38 -0300

    Update ShopApp.java

--- src/ShopApp.java
+++ src/ShopApp.java
@@ -1,15 +1,17 @@
+import java.util.Arrays;
+
 public class ShopApp {
     public static void main(String[] args) {
 
         double total = 0.0;
         int measurement = 8;
-        String separa = "****************************************************************************************";
+        String separator = "****************************************************************************************";
 
         System.out.println("Welcome to this simples Shop App");
 
         Customer c1 = new Customer();
 
-        System.out.println(separa); //Adição de items
+        System.out.println(separator); //Adição de items
         System.out.println("Minimum price: " + Clothing.minPrice);
         Clothing item1 = new Clothing("Blue Jacket", 20.9, "M");
         Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S");
@@ -20,16 +22,16 @@ public class ShopApp {
                 new Clothing("Blue T-Shirt", 10.5, "S")};
 
         c1.addItems(items);
-        System.out.println(separa); //Descrição do cliente
+        System.out.println(separator); //Descrição do cliente
         System.out.println("Customer is " + c1.getName() + ", " + c1.getSize() + ", " + c1.getTotalClothingCost());
-        System.out.println(separa); //Laço que imprime itens de compra
+        System.out.println(separator); //Laço que imprime itens de compra
         for (Clothing item : c1.getItems()) {
             System.out.println("Item: " + item);
         }
         int average = 0;
         int count = 0;
 
-        System.out.println(separa);//Laço para aumento de preço médio
+        System.out.println(separator);//Laço para aumento de preço médio
         for (Clothing item : c1.getItems()){
             if(item.getSize().equals("L")) {
                 count++;
@@ -42,7 +44,8 @@ public class ShopApp {
         } catch (ArithmeticException e) {
             System.out.println("Cant' divide " + average + " by " + count);
         }
+        Arrays.sort(c1.getItems());
         System.out.println("Total with tax is: " + total);
-        System.out.println(separa);
+        System.out.println(separator);
     }
 }
